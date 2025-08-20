d43d7aaedfec15a80969dea12ceeb33701aaa54e
Ascension2AnotherDimension <103389538+Ascension2AnotherDimension@users.noreply.github.com>
2025-08-20 11:57:26 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const links = document.querySelectorAll('nav ul li a');
+
+for(const link of links) {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const target = document.querySelector(link.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+}
